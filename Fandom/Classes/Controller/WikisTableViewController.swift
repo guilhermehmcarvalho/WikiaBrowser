856dc7f57932899c52ee5bc364d6a843074dd96b
guilhermehmcarvalho <guilhermehmcarvalho@gmail.com>
2018-06-07 12:34:48 +0200

    Fixed warnings and adde apibaseURL to workspace info

--- Fandom/Classes/Controller/WikisTableViewController.swift
+++ Fandom/Classes/Controller/WikisTableViewController.swift
@@ -56,18 +56,6 @@ class WikisTableViewController: UITableViewController {
     */
 
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
 
